7895a325ef2dedcfce7b93d4b7fd3dbeea974757
JaoSousa19 <58422053+JaoSousa9@users.noreply.github.com>
2024-01-07 16:51:04 +0000

    Initial set up with springboot - LiftOff

---
+++ src/test/java/com/liftOff/LiftOff/LiftOffApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liftOff.LiftOff;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiftOffApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
