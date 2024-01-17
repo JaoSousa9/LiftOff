f9e3be63b8a4fcc5b9abc480e552aa7bdc308448
Rita Azevedo <rita.m.azevedo4@gmail.com>
2024-01-17 23:23:48 +0000

    Passenger DTO with form validation

--- src/main/java/com/liftOff/LiftOff/command/PassengerDto.java
+++ src/main/java/com/liftOff/LiftOff/command/PassengerDto.java
@@ -1,4 +1,112 @@
 package com.liftOff.LiftOff.command;
 
+import jakarta.persistence.Column;
+
+import javax.validation.constraints.*;
+
 public class PassengerDto {
+
+    private Integer id;
+
+    @NotNull(message = "first name is mandatory")
+    @NotBlank(message = "first name is mandatory")
+    @Size(min = 3, max = 64)
+    private String firstName;
+
+    @NotNull(message = "last name is mandatory")
+    @NotBlank(message = "last name is mandatory")
+    @Size(min = 2, max = 64)
+    private String lastName;
+
+    @Email
+    @Column(unique = true)
+    private String email;
+
+    @NotNull(message = "username is mandatory")
+    @NotBlank(message = "username is mandatory")
+    @Column(unique = true)
+    private String username;
+
+    @NotNull(message = "password is mandatory")
+    @NotBlank(message = "password is mandatory")
+    @Size(min = 8, message = "password must be at least 8 characters")
+    @Pattern(
+            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).{8,}$",
+            message = "password must contain at least one digit, one lowercase and one uppercase letter"
+    )
+    private String password;
+
+    @Pattern(regexp = "^\\+?[0-9]*$", message = "phone number contains invalid characters")
+    @Size(min = 9, max = 16)
+    private String phoneNumber;
+
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PassengerDto{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                '}';
+    }
 }
