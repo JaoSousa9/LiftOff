c478f26e2ab33b53674d30c20767000af230d4a1
Rita Azevedo <rita.m.azevedo4@gmail.com>
2024-01-18 12:07:33 +0000

    Added passenger converters implementation - passenger and passenger dto

--- src/main/java/com/liftOff/LiftOff/converters/PassengerDtoToPassenger.java
+++ src/main/java/com/liftOff/LiftOff/converters/PassengerDtoToPassenger.java
@@ -1,19 +1,34 @@
 package com.liftOff.LiftOff.converters;
 
 import com.liftOff.LiftOff.command.PassengerDto;
+import com.liftOff.LiftOff.exceptions.PassengerNotFoundException;
 import com.liftOff.LiftOff.persistence.model.Passenger;
+import com.liftOff.LiftOff.services.PassengerService;
 import org.springframework.core.convert.converter.Converter;
 
 public class PassengerDtoToPassenger implements Converter<PassengerDto, Passenger> {
 
-    @Override
-    public Passenger convert(PassengerDto source) {
-        return null;
+    private PassengerService passengerService;
+
+
+    public void setPassengerService(PassengerService passengerService) {
+        this.passengerService = passengerService;
     }
 
     @Override
-    public <U> Converter<PassengerDto, U> andThen(Converter<? super Passenger, ? extends U> after) {
-        return Converter.super.andThen(after);
+    public Passenger convert(PassengerDto passengerDto) {
+
+        Passenger passenger = (passengerDto.getId() != null ? passengerService.get(passengerDto.getId()) : new Passenger());
+
+        passenger.setFirstName(passengerDto.getFirstName());
+        passenger.setLastName(passengerDto.getLastName());
+        passenger.setEmail(passengerDto.getEmail());
+        passenger.setUsername(passengerDto.getUsername());
+        passenger.setPassword(passengerDto.getPassword());
+        passenger.setPhoneNumber(passengerDto.getPhoneNumber());
+
+        return passenger;
     }
+
 
 }
